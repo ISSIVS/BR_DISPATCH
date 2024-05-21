2c2823a8a1a3f351c2849ed49d166eebefa18679
Pedro <pedrorocha9@yahoo.com>
2024-05-21 12:39:12 -0300

    node updated

--- install.bat
+++ install.bat
@@ -1,11 +1,18 @@
-set mypath= "%cd%
-cd %mypath%
-cd BR_GEA
-call create_db.bat
-SET F="deamon"
-IF EXIST %F% RMDIR  %F%
-node install.js
-node register.js
-echo Install successfull. 
 @echo off
+set "mypath=%cd%"
+
+REM Optionally change directory to BR_GEA if necessary
+REM cd BR_GEA
+
+call create_db.bat
+
+SET F="deamon"
+IF EXIST %F% (
+    RMDIR %F%
+)
+
+node.exe install.js
+node.exe register.js
+
+echo Install successful. 
 pause
